f2f8f0d31f98e864d6f1b349bb3004a3bb0c5e27
AnatolyKulinich <anatoly.k@payop.com>
2025-04-16 23:41:35 +0300

    setup theme

--- wp-content/themes/blogrock/functions.php
+++ wp-content/themes/blogrock/functions.php
@@ -1,2 +1,3 @@
 <?php
-require_once get_template_directory() . '/inc/register-blocks.php';
+require_once get_template_directory() . '/inc/acf.php';
+
---
+++ wp-content/themes/blogrock/index.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+    <main>
+        <h1>Hello from BlogRock!</h1>
+        <p>Это базовый шаблон index.php. Поздравляю, тема активна 🎉</p>
+    </main>
+
+<?php
+get_footer();
